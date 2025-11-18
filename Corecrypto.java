4f408b329c9fd90f743d9055dd87fa58617cd90a
Karnika Agarwal <159540684+karnikaagarwal@users.noreply.github.com>
2025-11-18 22:09:52 +0530

    Rename CliTool class to Corecrypto

--- Corecrypto.java
+++ Corecrypto.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 import java.util.Base64;
 import java.util.Arrays;
 
-public class CliTool {
+public class Corecrypto {
 public static void main(String[] args) throws Exception {
 if (args.length < 1) {
 printUsageAndExit();
